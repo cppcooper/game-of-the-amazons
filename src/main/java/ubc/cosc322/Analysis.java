9048f61a1904b80bcbef81ebdc4d664eeb520436
Josh Cooper <cooper.s.josh@outlook.com>
2021-02-21 22:14:33 -0800

    Implements the core of move scanning

--- src/main/java/ubc/cosc322/Analysis.java
+++ src/main/java/ubc/cosc322/Analysis.java
@@ -5,7 +5,9 @@ import java.util.function.*;
 
 public class Analysis {
     public static ArrayList<Integer>[] GetMoveList(LocalState board, BoardPiece[] player_pieces){
+        //todo: type needs to change (I think)
         ArrayList<Integer>[] all_moves = new ArrayList[4];
+        //todo: parallelize below this using a sync-list. At this level, just refactor and pass the element(?) pass by value.. will it work?
         for(int i = 0; i < 4; ++i){
             all_moves[i] = ScanMoves(board,player_pieces[i]);
         }
@@ -17,6 +19,7 @@ public class Analysis {
         int xi = 0;
         int yi = 0;
 
+        //todo: launch all of these as threads
         xi = 1; yi = 1;
         moves.addAll(ScanMoves(board,piece,xi,yi));
         xi = -1; yi = -1;
@@ -42,11 +45,16 @@ public class Analysis {
 
     protected static ArrayList<Integer> ScanMoves(LocalState board, BoardPiece piece, int xi, int yi){
         ArrayList<Integer> moves = new ArrayList<>();
-        int x = piece.x;
-        int y = piece.y;
-        int index;
-
-        while(x == 1000){
+        int x = piece.x + xi;
+        int y = piece.y + yi;
+        Function<Integer, Boolean> check_in_range = (v) -> {
+            return (v < 11 && v > 0);
+        };
+        while(check_in_range.apply(x) && check_in_range.apply(y)){
+            if(board.ReadTile(x,y) != 0){
+                break;
+            }
+            moves.add((x*11)+y);
             x += xi;
             y += yi;
         }
