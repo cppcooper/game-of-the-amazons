36c29926d13f859038c1278526d9f4dc10274764
Josh Cooper <cooper.s.josh@outlook.com>
2021-02-23 11:42:44 -0800

    Moves around some code inside Analysis

--- src/main/java/ubc/cosc322/Analysis.java
+++ src/main/java/ubc/cosc322/Analysis.java
@@ -18,6 +18,7 @@ public class Analysis {
     }
 
     //25600 bottom level operations if used on the 320 new positions from above
+    //This should hopefully be adequate for parallelization I think splitting into 8 sections should be adequate work for each thread based on benchmarking, but thorough testing is required
     public static ArrayList<Integer>[] GetOpenPositions(LocalState board, ArrayList<Integer> starting_positions){
         ArrayList<Integer>[] all_moves = new ArrayList[starting_positions.size()];
         for(int i = 0; i < starting_positions.size(); ++i){
@@ -26,20 +27,42 @@ public class Analysis {
         return all_moves;
     }
 
-    public static ArrayList<Integer>[] GetMoveListThreaded(LocalState board, BoardPiece[] player_pieces) throws ExecutionException, InterruptedException {
-        ArrayList<Integer>[] all_moves = new ArrayList[4];
-        Future<ArrayList<Integer>>[] ret_values = new Future[4];
-        for(int i = 0; i < 4; ++i){
-            BoardPiece piece = player_pieces[i];
-            ret_values[i] = thread_manager.submit(() -> ScanMoves(board,piece.pos));
-        }
-        for(int i = 0; i < 4; ++i){
-            all_moves[i] = ret_values[i].get();
-        }
-        return all_moves;
-    }
+    // This is not faster, but the code is useful for syntax lookup purposes
+//    public static ArrayList<Integer>[] GetMoveListThreaded(LocalState board, BoardPiece[] player_pieces) throws ExecutionException, InterruptedException {
+//        ArrayList<Integer>[] all_moves = new ArrayList[4];
+//        Future<ArrayList<Integer>>[] ret_values = new Future[4];
+//        for(int i = 0; i < 4; ++i){
+//            BoardPiece piece = player_pieces[i];
+//            ret_values[i] = thread_manager.submit(() -> ScanMoves(board,piece.pos));
+//        }
+//        for(int i = 0; i < 4; ++i){
+//            all_moves[i] = ret_values[i].get();
+//        }
+//        return all_moves;
+//    }
 
     ///Helper functions
+
+    protected static ArrayList<Integer> ScanMoves(LocalState board, int index){
+        return ScanMoves(board,new Position(index));
+    }
+
+    //this will always be faster than a parallel version for the board size we have
+    protected static ArrayList<Integer> ScanMoves(LocalState board, Position pos){
+        Integer[] moves = new Integer[80];
+
+        ScanDirection(moves,0,board,pos.x,pos.y,1,1);
+        ScanDirection(moves,10,board,pos.x,pos.y,-1,-1);
+        ScanDirection(moves,20,board,pos.x,pos.y,1,-1);
+        ScanDirection(moves,30,board,pos.x,pos.y,-1,1);
+
+        ScanDirection(moves,40,board,pos.x,pos.y,1,0);
+        ScanDirection(moves,50,board,pos.x,pos.y,-1,0);
+        ScanDirection(moves,60,board,pos.x,pos.y,0,1);
+        ScanDirection(moves,70,board,pos.x,pos.y,0,-1);
+
+        return new ArrayList<>(Arrays.asList(moves));
+    }
 
     //this has been optimized to death
     protected static void ScanDirection(Integer[] moves, int start_index, LocalState board, int x, int y, int xi, int yi){
@@ -61,26 +84,5 @@ public class Analysis {
         while(i < start_index + 10){
             moves[i++] = -1;
         }
-    }
-
-    protected static ArrayList<Integer> ScanMoves(LocalState board, int index){
-        return ScanMoves(board,new Position(index));
-    }
-
-    //this will always be faster than a parallel version for the board size we have
-    protected static ArrayList<Integer> ScanMoves(LocalState board, Position pos){
-        Integer[] moves = new Integer[80];
-
-        ScanDirection(moves,0,board,pos.x,pos.y,1,1);
-        ScanDirection(moves,10,board,pos.x,pos.y,-1,-1);
-        ScanDirection(moves,20,board,pos.x,pos.y,1,-1);
-        ScanDirection(moves,30,board,pos.x,pos.y,-1,1);
-
-        ScanDirection(moves,40,board,pos.x,pos.y,1,0);
-        ScanDirection(moves,50,board,pos.x,pos.y,-1,0);
-        ScanDirection(moves,60,board,pos.x,pos.y,0,1);
-        ScanDirection(moves,70,board,pos.x,pos.y,0,-1);
-
-        return new ArrayList<>(Arrays.asList(moves));
     }
 }
