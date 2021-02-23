95672df891a2cc2e2685cca5f4c54b6d503c82f7
Josh Cooper <cooper.s.josh@outlook.com>
2021-02-23 11:37:09 -0800

    Improves Analysis class further

--- src/main/java/ubc/cosc322/Analysis.java
+++ src/main/java/ubc/cosc322/Analysis.java
@@ -1,78 +1,37 @@
 package ubc.cosc322;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.*;
-import java.util.stream.Collectors;
 
 public class Analysis {
-    static private ExecutorService thread_manager = Executors.newCachedThreadPool(); //Supposedly good for short lived concurrent tasks
+    static private ExecutorService thread_manager = Executors.newCachedThreadPool();
 
+    //320 bottom level operations -> new positions to look at
     public static ArrayList<Integer>[] GetMoveList(LocalState board, BoardPiece[] player_pieces){
         ArrayList<Integer>[] all_moves = new ArrayList[4];
         for(int i = 0; i < 4; ++i){
-            all_moves[i] = ScanMoves(board,player_pieces[i]);
+            all_moves[i] = ScanMoves(board,player_pieces[i].pos);
         }
         return all_moves;
     }
 
-    protected static ArrayList<Integer> ScanMoves(LocalState board, BoardPiece piece){
-        ArrayList<Integer> moves = new ArrayList<>();
-        int xi = 0;
-        int yi = 0;
-
-        //todo: launch all of these as threads
-        xi = 1; yi = 1;
-        ScanMoves(moves,board,piece,xi,yi);
-        xi = -1; yi = -1;
-        ScanMoves(moves,board,piece,xi,yi);
-
-        xi = 1; yi = 0;
-        ScanMoves(moves,board,piece,xi,yi);
-        xi = -1; yi = 0;
-        ScanMoves(moves,board,piece,xi,yi);
-
-        xi = 0; yi = 1;
-        ScanMoves(moves,board,piece,xi,yi);
-        xi = 0; yi = -1;
-        ScanMoves(moves,board,piece,xi,yi);
-
-        xi = 1; yi = -1;
-        ScanMoves(moves,board,piece,xi,yi);
-        xi = -1; yi = 1;
-        ScanMoves(moves,board,piece,xi,yi);
-
-        return moves;
-    }
-
-    protected static void ScanMoves(ArrayList<Integer> moves, LocalState board, BoardPiece piece, int xi, int yi){
-        int x = piece.x + xi;
-        int y = piece.y + yi;
-        Function<Integer, Boolean> check_in_range = (v) -> {
-            return (v < 11 && v > 0);
-        }; //hoping the JVM is gonna inline this =)
-        while(check_in_range.apply(x) && check_in_range.apply(y)){
-            if(board.ReadTile(x,y) != 0){
-                break;
-            }
-            moves.add((x*11)+y);
-            x += xi;
-            y += yi;
+    //25600 bottom level operations if used on the 320 new positions from above
+    public static ArrayList<Integer>[] GetOpenPositions(LocalState board, ArrayList<Integer> starting_positions){
+        ArrayList<Integer>[] all_moves = new ArrayList[starting_positions.size()];
+        for(int i = 0; i < starting_positions.size(); ++i){
+            all_moves[i] = ScanMoves(board,starting_positions.get(i));
         }
+        return all_moves;
     }
-
-    ///Threaded variants
-    //todo: benchmark threaded vs non-threaded
 
     public static ArrayList<Integer>[] GetMoveListThreaded(LocalState board, BoardPiece[] player_pieces) throws ExecutionException, InterruptedException {
         ArrayList<Integer>[] all_moves = new ArrayList[4];
         Future<ArrayList<Integer>>[] ret_values = new Future[4];
         for(int i = 0; i < 4; ++i){
             BoardPiece piece = player_pieces[i];
-            ret_values[i] = thread_manager.submit(() -> ScanMovesThreaded(board,piece));
+            ret_values[i] = thread_manager.submit(() -> ScanMoves(board,piece.pos));
         }
         for(int i = 0; i < 4; ++i){
             all_moves[i] = ret_values[i].get();
@@ -80,55 +39,48 @@ public class Analysis {
         return all_moves;
     }
 
-    protected static ArrayList<Integer> ScanMovesThreaded(LocalState board, BoardPiece piece){
-        ArrayList<Integer> moves = new ArrayList<>();
-        ArrayList<Future<ArrayList<Integer>>> ret_values = new ArrayList<>(8);
+    ///Helper functions
 
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,1,1)));
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,-1,-1)));
-
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,1,0)));
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,-1,0)));
-
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,0,1)));
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,0,-1)));
-
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,1,-1)));
-        ret_values.add(thread_manager.submit(() -> ScanMoves4Threads(board,piece,-1,1)));
-
-        ret_values.stream().parallel()
-            .map(f -> {
-                try {
-                    return f.get();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return null;
-            })
-            .map(c -> (List<Integer>)c)
-            .reduce(Collections.synchronizedList(moves),
-                (v1,v2) -> {
-                    v1.addAll(v2);
-                    return v1;
-            });
-        return moves;
-    }
-
-    protected static ArrayList<Integer> ScanMoves4Threads(LocalState board, BoardPiece piece, int xi, int yi){
-        ArrayList<Integer> moves = new ArrayList<>();
-        int x = piece.x + xi;
-        int y = piece.y + yi;
+    //this has been optimized to death
+    protected static void ScanDirection(Integer[] moves, int start_index, LocalState board, int x, int y, int xi, int yi){
+        x += xi;
+        y += yi;
+        int i = start_index;
         Function<Integer, Boolean> check_in_range = (v) -> {
             return (v < 11 && v > 0);
         }; //hoping the JVM is gonna inline this =)
         while(check_in_range.apply(x) && check_in_range.apply(y)){
-            if(board.ReadTile(x,y) != 0){
+            int index = (x*11)+y;
+            if(board.ReadTile(index) != 0){
                 break;
             }
-            moves.add((x*11)+y);
+            moves[i++] = index;
             x += xi;
             y += yi;
         }
-        return moves;
+        while(i < start_index + 10){
+            moves[i++] = -1;
+        }
+    }
+
+    protected static ArrayList<Integer> ScanMoves(LocalState board, int index){
+        return ScanMoves(board,new Position(index));
+    }
+
+    //this will always be faster than a parallel version for the board size we have
+    protected static ArrayList<Integer> ScanMoves(LocalState board, Position pos){
+        Integer[] moves = new Integer[80];
+
+        ScanDirection(moves,0,board,pos.x,pos.y,1,1);
+        ScanDirection(moves,10,board,pos.x,pos.y,-1,-1);
+        ScanDirection(moves,20,board,pos.x,pos.y,1,-1);
+        ScanDirection(moves,30,board,pos.x,pos.y,-1,1);
+
+        ScanDirection(moves,40,board,pos.x,pos.y,1,0);
+        ScanDirection(moves,50,board,pos.x,pos.y,-1,0);
+        ScanDirection(moves,60,board,pos.x,pos.y,0,1);
+        ScanDirection(moves,70,board,pos.x,pos.y,0,-1);
+
+        return new ArrayList<>(Arrays.asList(moves));
     }
 }
