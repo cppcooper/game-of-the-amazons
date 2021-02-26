f8c3c6a20adc3a3855dd4b19b1b5502570087be5
Josh Cooper <cooper.s.josh@outlook.com>
2021-02-26 13:27:45 -0800

    Adds start member to Move

--- src/main/java/ubc/cosc322/Move.java
+++ src/main/java/ubc/cosc322/Move.java
@@ -2,16 +2,18 @@ package ubc.cosc322;
 
 //Stores indices of a piece and arrow, can be used to represent a Move
 public class Move {
+    public int start = -1;
     public int piece = -1;
     public int arrow = -1;
     public Move prev_move = null;
     public Move next_move = null;
-    Move(int piece, int arrow){
+    Move(int start, int piece, int arrow){
+        this.start = start;
         this.piece = piece;
         this.arrow = arrow;
     }
-    Move(int piece, int arrow, Move prev){
-        this(piece,arrow);
+    Move(int start, int piece, int arrow, Move prev){
+        this(start,piece,arrow);
         prev_move = prev;
     }
 }
