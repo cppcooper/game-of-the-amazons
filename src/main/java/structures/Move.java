5518c7d8e80081d1d154dd34ca099c17d0f0de24
Josh Cooper <cooper.s.josh@outlook.com>
2021-02-27 20:51:34 -0800

    Implements IsValidFor(LocalState) method in Move

--- src/main/java/structures/Move.java
+++ src/main/java/structures/Move.java
@@ -16,4 +16,12 @@ public class Move {
         this(start,piece,arrow);
         prev_move = prev;
     }
+    public boolean IsValidFor(LocalState state){
+        int start_pos = state.ReadTile(start);
+        if(start_pos > 0 && start_pos < 3){
+            // return true if both tiles are empty (ie. equal to zero)
+            return 0 == (state.ReadTile(piece) | state.ReadTile(arrow));
+        }
+        return false;
+    }
 }
