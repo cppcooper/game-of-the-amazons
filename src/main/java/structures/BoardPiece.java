91cd4eb6cceb6b9d8bee668096e127a5573b6652
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-02 11:24:51 -0800

    Implements copy constructors for LocalState/BoardPiece/Position

--- src/main/java/structures/BoardPiece.java
+++ src/main/java/structures/BoardPiece.java
@@ -4,6 +4,10 @@ public class BoardPiece {
     public Position pos;
     public int player; //3 if non-player or 0 if not a piece
 
+    public BoardPiece(BoardPiece other){
+        pos = new Position(other.pos);
+        player = other.player;
+    }
     public BoardPiece(int index, int player){
         pos = new Position(index);
         this.player = player;
--- src/main/java/structures/Position.java
+++ src/main/java/structures/Position.java
@@ -3,6 +3,9 @@ package structures;
 public class Position {
     public int x;
     public int y;
+    public Position(Position other){
+        this(other.x,other.y);
+    }
     public Position(int x, int y){
         this.x = x;
         this.y = y;
