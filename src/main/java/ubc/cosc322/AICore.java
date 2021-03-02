678d5291ed253f4a611c5d721a4d4f649df2092f
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-02 13:46:13 -0800

    Adds AICore with some basic implementation details

---
+++ src/main/java/ubc/cosc322/AICore.java
@@ -0,0 +1,54 @@
+package ubc.cosc322;
+
+import structures.LocalState;
+import structures.Move;
+import structures.Position;
+import ygraph.ai.smartfox.games.amazons.AmazonsBoard;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AICore {
+    private static LocalState current_board_state;
+
+    public static synchronized void set(){}
+    public static synchronized void update(){}
+    public static synchronized void read(){}
+
+    public static void run(){
+
+    }
+
+    public static synchronized void SetState(ArrayList<Integer> state){
+        try {
+            current_board_state = new LocalState(state,true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized void UpdateState(final Map<String, Object> msgDetails) {
+        ArrayList<Integer> qcurr = (ArrayList)msgDetails.get("queen-position-current");
+        ArrayList<Integer> qnew = (ArrayList)msgDetails.get("queen-position-next");
+        ArrayList<Integer> arrow = (ArrayList)msgDetails.get("arrow-position");
+        Move move = new Move(
+                Position.CalculateIndex(qcurr.get(0),qcurr.get(1)),
+                Position.CalculateIndex(qnew.get(0),qnew.get(1)),
+                Position.CalculateIndex(arrow.get(0),arrow.get(1)));
+        current_board_state.MakeMoves(move,true);
+        PruneGameTree();
+    }
+
+    private static synchronized LocalState GetState(){
+        return current_board_state;
+    }
+
+    public static Move GetBestMove(){
+        return null;
+    }
+
+    private static void PruneGameTree(){
+
+    }
+}
