d06fe6f134c3032b1fc871d0672ef3fdc93f8401
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-03 14:54:29 -0800

    Implements the basic form of MonteCarlo simulation

--- src/main/java/structures/GameTreeNode.java
+++ src/main/java/structures/GameTreeNode.java
@@ -5,4 +5,7 @@ import java.util.ArrayList;
 public class GameTreeNode {
     public Move move = null;
     public ArrayList<GameTreeNode> next_nodes = new ArrayList<>();
+    public GameTreeNode(Move move){
+        this.move = move;
+    }
 }
