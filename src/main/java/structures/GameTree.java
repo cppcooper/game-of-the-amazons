df9c72f68b7ff9acae80e0762ab82dceb4813898
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-03 11:05:16 -0800

    Refactors GameTree

--- src/main/java/structures/GameTree.java
+++ src/main/java/structures/GameTree.java
@@ -1,19 +1,35 @@
 package structures;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GameTree {
-    private static ConcurrentHashMap<LocalState,GameTreeNode> game_tree = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<Integer,
+            ConcurrentHashMap<LocalState,GameTreeNode>> game_tree = new ConcurrentHashMap<>();
+    private static int hash_prune_mask = (256-1) << 24;
 
     public static boolean put(LocalState board, GameTreeNode node){
-        if(!game_tree.containsKey(board)){
-            game_tree.put(board,node);
+        int hash_code = board.hashCode();
+        int hash_8bit = hash_code & hash_prune_mask;
+        var inner_map = game_tree.get(board.hashCode() & hash_prune_mask);
+        if(inner_map == null) {
+            inner_map = new ConcurrentHashMap<LocalState,GameTreeNode>();
+            game_tree.put(hash_8bit, inner_map);
+            inner_map.put(board,node);
+            return true;
+        }
+        if(!inner_map.containsKey(board)){
+            inner_map.put(board,node);
             return true;
         }
         return false;
     }
 
     public static GameTreeNode get(LocalState board){
-        return game_tree.get(board);
+        var inner_map = game_tree.get(board.hashCode() & hash_prune_mask);
+        if(inner_map != null){
+            return inner_map.get(board);
+        }
+        return null;
     }
 }
