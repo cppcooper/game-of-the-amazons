1eb1cf71816b822f972d5760e83da6f76ac0a7e0
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-03 19:52:23 -0800

    Updates GameTree put method

--- src/main/java/structures/GameTree.java
+++ src/main/java/structures/GameTree.java
@@ -1,25 +1,20 @@
 package structures;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GameTree {
     private static final ConcurrentHashMap<Integer,
             ConcurrentHashMap<LocalState,GameTreeNode>> game_tree = new ConcurrentHashMap<>();
 
-    public static boolean put(LocalState board, GameTreeNode node){
+    public static void put(LocalState board, GameTreeNode node){
         var inner_map = game_tree.get(board.GetMoveNumber());
         if(inner_map == null) {
-            inner_map = new ConcurrentHashMap<LocalState,GameTreeNode>();
+            inner_map = new ConcurrentHashMap<>();
             game_tree.put(board.GetMoveNumber(), inner_map);
             inner_map.put(board,node);
-            return true;
-        }
-        if(!inner_map.containsKey(board)){
+        } else if(!inner_map.containsKey(board)){
             inner_map.put(board,node);
-            return true;
         }
-        return false;
     }
 
     public static GameTreeNode get(LocalState board){
