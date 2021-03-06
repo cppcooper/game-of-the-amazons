56218ffc2c31402ef7b95ae1d0fc4c2a513b0ce1
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-06 14:40:08 -0800

    Revises part of Main.java

--- src/main/java/ubc/cosc322/Main.java
+++ src/main/java/ubc/cosc322/Main.java
@@ -8,27 +8,28 @@ public class Main {
 
     public static void main(String[] args){
         if(args.length >= 2) {
-            player = new Player(args[0], args[1]);
-            BaseGameGUI.sys_setup();
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    player.Go();
-                }
-            });
-            Thread ai_thread = new Thread(() -> AICore.run());
-            ai_thread.run();
-
-            // todo: write game loop
-            //while(...)
-            // todo: determine game over condition/event/etc.
-            // todo: figure out how to wait/start new game after game over
-            while(player.our_turn.get()){
-                //todo: sleep up to 29.5s
-                Move m = AICore.GetBestMove();
-                //todo: 1) construct move message. 2) send move message
-                //player.getGameClient().sendMoveMessage(...);
-            }
             try {
+                player = new Player(args[0], args[1]);
+                BaseGameGUI.sys_setup();
+                java.awt.EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        player.Go();
+                    }
+                });
+                Thread ai_thread = new Thread(() -> AICore.run());
+                ai_thread.run();
+
+                // todo: write game loop
+                //while(...)
+                // todo: determine game over condition/event/etc.
+                // todo: figure out how to wait/start new game after game over
+                while(player.our_turn.get()){
+                    //todo: sleep up to 29.5s
+                    Move m = AICore.GetBestMove();
+                    //todo: 1) construct move message. 2) send move message
+                    //player.getGameClient().sendMoveMessage(...);
+                }
+
                 ai_thread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
