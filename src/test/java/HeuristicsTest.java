a8b3c3279a5169af63b8dd65b048139b45ec49e7
sara <sara@Tree_T-Rex.localdomain>
2021-03-09 08:49:46 -0800

    HeuristicsTest.java is now a Junit test

--- src/test/java/HeuristicsTest.java
+++ src/test/java/HeuristicsTest.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import structures.LocalState;
 import algorithms.analysis.Heuristics;
 
-//todo: refactor test class to use Heuristics class's code (directly)
+
 public class HeuristicsTest {
 
-	// These are the methods that the Heuristics class uses. It's kinda useful if you 
-	// want to see what's going on with the board when you use the methods, but delete this if you want.
-
-	//todo: revise to not have main() but instead use junit test. See AnalysisTest for an example
-	public static void main(String[] args) throws Exception {
-
+	@Test
+	void TestHeuristics() {
 		// 0 is open space
 		// 1 is player 1 pieces
 		// 2 is player 2 pieces
@@ -30,18 +29,27 @@ public class HeuristicsTest {
 				0, 1, 3, 3, 0, 0, 0, 3, 0, 0, 1, 
 				0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 
 				0, 0, 0, 1, 3, 3, 0, 3, 0, 0, 0};
-
-		//Creates new LocalState, sets LocalState board to board shown above
-		LocalState state = new LocalState(new ArrayList<Integer>(Arrays.asList(board)), true);
-		state.DebugPrint();
-
+		
 		int index = 72; //position 6,6
+		
+		try {
+			
+			//Creates new LocalState, sets LocalState board to board shown above
+			LocalState state = new LocalState(new ArrayList<Integer>(Arrays.asList(board)), true);
+			state.DebugPrint();
+			Heuristics.CountData info = Heuristics.GetCount(state, index);
 
-		Heuristics.CountData info = Heuristics.GetCount(state, index);
-
-		System.out.println(info.blanks);
-		System.out.println(info.blocks);
-		System.out.println(info.blocks_heuristic);
-
+			System.out.println(info.blanks);
+			System.out.println(info.blocks);
+			System.out.println(info.blocks_heuristic);
+			
+			assertEquals(42, info.blanks);
+			assertEquals(32, info.blocks);
+			assertEquals(15.529999999999992, info.blocks_heuristic);
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
