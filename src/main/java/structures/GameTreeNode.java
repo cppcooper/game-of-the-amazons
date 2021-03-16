e1f6ec50bcdefb1235528958e05bcec0569ef98a
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-15 20:10:07 -0700

    Adds heuristic aggregation fields to GameTreeNode

--- src/main/java/structures/GameTreeNode.java
+++ src/main/java/structures/GameTreeNode.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class GameTreeNode {
     public double heuristic = -1.0;
+    public double aggregate = -1.0;
+    public int aggregation_depth = 0;
     public Move move = null;
     public GameTreeNode super_node = null;
     protected ArrayList<GameTreeNode> sub_nodes = new ArrayList<>(); //note: that there is no way to remove nodes! this is by design!
