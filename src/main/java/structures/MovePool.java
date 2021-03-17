de8be167e396d646676e6d3f4fada34c386950cd
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-16 21:35:01 -0700

    Adds MovePool.generate_pool()

--- src/main/java/structures/MovePool.java
+++ src/main/java/structures/MovePool.java
@@ -1,11 +1,30 @@
 package structures;
 
+import algorithms.analysis.MoveCompiler;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
+import tools.RandomGen;
 
 import java.util.HashMap;
 
 public class MovePool {
     private static HashMap<Integer, Move> pool = new HashMap<>(64000, 3.f);
+
+    public static void generate_pool(){
+        RandomGen rng = new RandomGen();
+        BoardPiece[] all_positions = new BoardPiece[100];
+        int i = 0;
+        for(int x = 1; x < 11; ++x){
+            for(int y = 1; y < 11; ++y){
+                Position p = new Position(x,y);
+                if(p.IsValid()){
+                    all_positions[i++] = new BoardPiece(p.x,p.y,1);
+                }
+            }
+        }
+        for(int trial = 0; trial < 6400; ++trial){ //this should get alot, if not most possible Moves
+            MoveCompiler.GetMoveList(new LocalState(rng.GetRandomState(0.35),false,true),all_positions,true);
+        }
+    }
 
     private static int make_key(int start, int next, int arrow){
         final int bits = 8;
