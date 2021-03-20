37d13c46828b49886b1170bc22dd43c0e3a512c7
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-20 13:03:58 -0700

    Fixes Sequence generation code

--- src/test/java/RNGbench.java
+++ src/test/java/RNGbench.java
@@ -8,21 +8,12 @@ public class RNGbench {
         Benchmarker B = new Benchmarker();
         RandomGen rng = new RandomGen();
         final int N = 10000;
-        System.out.printf("Simple Seq: %7d ns\n", AverageRuntime(B, N, () -> rng.GetSimpleSequence(0, 1000)));
-        System.out.printf("Random Seq: %7d ns\n", AverageRuntime(B, N, () -> rng.GetRandomSequenceShuffled(0, 1000, 1000)));
+        System.out.printf("Random Seq: %7d ns\n", B.AverageRuntime(B, N, () -> rng.GetRandomSequenceShuffled(0, 1000, 1000)));
         System.out.println("Distinct Value Sequences");
-        System.out.printf("Looped Seq: %7d ns\n", AverageRuntime(B, N, () -> rng.GetDistinctSequenceShuffled(0, 1000, 1000)));
-        System.out.printf("Stream Seq: %7d ns\n", AverageRuntime(B, N, () -> rng.DEPRECATED_GetDistinctSequenceShuffled(0, 1000, 1000)));
+        System.out.printf("Number Seq: %7d ns\n", B.AverageRuntime(B, N, () -> rng.GetNumberSequence(0, 1000)));
+        System.out.printf("Looped Seq: %7d ns\n", B.AverageRuntime(B, N, () -> rng.GetDistinctSequenceShuffled(0, 1000, 1000)));
+        System.out.printf("Stream Seq: %7d ns\n", B.AverageRuntime(B, N, () -> rng.DEPRECATED_GetDistinctSequenceShuffled(0, 1000, 1000)));
     }
 
-    long AverageRuntime(Benchmarker B, int N, Runnable fn){
-        long total = 0;
-        for(int i = 0; i < N; ++i){
-            B.Start();
-            fn.run();
-            B.Stop();
-            total += B.ElapsedNano();
-        }
-        return total / N;
-    }
+
 }
