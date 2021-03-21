9c450c33f002a7201f7b01744397c3863a204878
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-21 16:34:47 -0700

    Adds aggregate counter to GameTreeNode

--- src/main/java/structures/GameTreeNode.java
+++ src/main/java/structures/GameTreeNode.java
@@ -1,17 +1,18 @@
 package structures;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class GameTreeNode {
     private final SynchronizedArrayList<GameTreeNode> super_nodes = new SynchronizedArrayList<>();
     private final SynchronizedArrayList<GameTreeNode> sub_nodes = new SynchronizedArrayList<>(); //note: that there is no way to remove nodes! this is by design!
 
-    private int N = 0;
+    private final AtomicInteger N = new AtomicInteger(0);
     private final AtomicDouble heuristic = new AtomicDouble();
     final public AtomicDouble aggregate_heuristic = new AtomicDouble();
+    final public AtomicInteger aggregate_count = new AtomicInteger(0);
     final public AtomicBoolean has_first_degree = new AtomicBoolean(false);
     final public AtomicBoolean has_count = new AtomicBoolean(false);
     final public AtomicBoolean has_territory = new AtomicBoolean(false);
@@ -37,7 +38,8 @@ public class GameTreeNode {
         double this_aggregate = aggregate_heuristic.get();
         if(this_aggregate > 0.f) {
             double parents_new_aggregate = parent.aggregate_heuristic.get() + this_aggregate;
-            parent.propagate(parents_new_aggregate);
+            int parents_new_aggregate_count = parent.aggregate_count.get() + aggregate_count.get();
+            parent.update_aggregate(parents_new_aggregate,parents_new_aggregate_count);
         }
     }
 
@@ -62,30 +64,37 @@ public class GameTreeNode {
 
     public void add_heuristic(double new_value){
         //newMean = oldMean + (Data - oldMean) / N;
+        assert new_value >= 0;
         double h = heuristic.get();
         double new_aggregate = aggregate_heuristic.get() - h;
-        h = h + (new_value - h) / ++N;
+        int new_aggregate_count = aggregate_count.get() + (this.N.get() == 0 ? 1 : 0);
+        h = h + (new_value - h) / N.incrementAndGet();
         heuristic.set(h);
         new_aggregate += h;
-        propagate(new_aggregate);
+        update_aggregate(new_aggregate, new_aggregate_count);
     }
 
     public void set_heuristic(double new_value, int N){
-        this.N = N;
+        assert new_value >= 0;
+        int new_aggregate_count = aggregate_count.get() + (this.N.get() == 0 ? 1 : 0);
         double new_aggregate = aggregate_heuristic.get() - heuristic.get() + new_value;
+        this.N.set(N);
         heuristic.set(new_value);
-        propagate(new_aggregate);
+        update_aggregate(new_aggregate,new_aggregate_count);
     }
 
-    public void propagate(double new_aggregate){
+    public void update_aggregate(double new_aggregate, int new_aggregate_count){
         // todo (1): consider changing the weighting of aggregation (currently 1:1 ratio; parent:child)
         // todo (debug): verify this function, and its uses ensure an unbroken chain of heuristic aggregating
+        int delta_count = new_aggregate_count - aggregate_count.get();
         double delta_aggregate = new_aggregate - aggregate_heuristic.get();
         aggregate_heuristic.set(new_aggregate);
+        aggregate_count.set(new_aggregate_count);
         for(int i = 0; i < super_nodes.size(); ++i){
             GameTreeNode parent = super_nodes.get(i);
             double parents_new_aggregate = parent.aggregate_heuristic.get() + delta_aggregate;
-            parent.propagate(parents_new_aggregate);
+            int parents_new_aggregate_count = parent.aggregate_count.get() + delta_count;
+            parent.update_aggregate(parents_new_aggregate,parents_new_aggregate_count);
         }
     }
 
