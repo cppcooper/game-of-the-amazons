72be1b1b7bc2a6bd3270201a60575792e063dbd3
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-22 14:43:48 -0700

    Removes some defunct code

--- src/test/java/algorithms/analysis/HeuristicsTest.java
+++ src/test/java/algorithms/analysis/HeuristicsTest.java
@@ -1,9 +1,5 @@
 package algorithms.analysis;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.apache.commons.math3.util.Precision;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -49,15 +45,15 @@ public class HeuristicsTest {
 		}
 		System.out.println("Single piece counting heuristics..");
 		System.out.printf("empty (1st degree): %d\n", first_degree);
-		System.out.printf("empty: %d\n",info.empty);
-		System.out.printf("nonempty: %d\n", info.nonempty);
-		System.out.printf("raw empty heuristic: %.1f\n", info.empty_heuristic);
-		System.out.printf("raw nonempty heuristic: %.1f\n", info.nonempty_heuristic);
+		System.out.printf("empty: %d\n",info.our_empty);
+		System.out.printf("nonempty: %d\n", info.our_nonempty);
+		//System.out.printf("raw empty heuristic: %.1f\n", info.empty_heuristic);
+		System.out.printf("raw nonempty heuristic: %.1f\n", info.our_nonempty_weighted);
 		System.out.println("#################\nScaled heuristics");
-		System.out.printf("empty: %.3f\nnonempty: %.3f\n", info.empty_heuristic/161, info.nonempty_heuristic/80);
+		//System.out.printf("empty: %.3f\nnonempty: %.3f\n", info.empty_heuristic/161, info.nonempty_heuristic/80);
 
-		assertEquals(42, info.empty);
-		assertEquals(32, info.nonempty);
+		assertEquals(42, info.our_empty);
+		assertEquals(32, info.our_nonempty);
 		//assertEquals(true, Precision.equals(15.475,info.nonempty_heuristic,0.001));
 	}
 }
