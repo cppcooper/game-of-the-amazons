a42c02d20526b2e5ccddcbf2b74c21aaeafe1d87
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-22 14:47:01 -0700

    Refactors/Revises Heuristics class

--- src/main/java/algorithms/analysis/Heuristics.java
+++ src/main/java/algorithms/analysis/Heuristics.java
@@ -94,11 +94,42 @@ public class Heuristics {
 
 	public static double GetFirstDegreeMoveHeuristic(LocalState board){
 		int value = GetFirstDegreeMoveCount(board);
-		return (double)value / (4*35);
+		// the less moves the other player has the higher the returned heuristic
+		return (1-(double)value / (4*35));
 	}
 
+	public static double GetCountHeuristic(LocalState board){
+		BoardPiece[] our_pieces = board.GetPrevTurnPieces(); // we'll calculate heuristics for the player who got us here
+		CountData total = new CountData();
+		for (int i = 0; i < 4; ++i) {
+			int index = our_pieces[i].pos.CalculateIndex();
+			total.add_our_counts(GetCount(board,index));
+		}
+		BoardPiece[] their_pieces = board.GetTurnPieces(); // we'll calculate heuristics for the player who got us here
+		for (int i = 0; i < 4; ++i) {
+			int index = their_pieces[i].pos.CalculateIndex();
+			total.add_their_counts(GetCount(board,index));
+		}
+		double empty_heuristic = (double)total.our_empty / (total.our_empty + total.their_empty);
+		double nonempty_heuristic = total.our_nonempty_weighted / (total.our_nonempty_weighted + total.their_nonempty_weighted);
+		double ret_heuristic = empty_heuristic - nonempty_heuristic;
+		return ASingleMaths.remap_value(ret_heuristic,-0.6,0.75,0,1);
+	}
+
+	public static double GetTerritoryHeuristic(LocalState board){
+		var counts = GetTerritoryCount(board);
+		int total = counts.ours + counts.theirs;
+		double heuristic = 3 * ((double)counts.ours / total);
+		heuristic = Math.pow(heuristic,2);
+		heuristic /= 9;
+		return heuristic;
+	}
+
+	////////////////////////////
+	/// Nuts and Bolts methods
+
 	public static int GetFirstDegreeMoveCount(LocalState board){
-		BoardPiece[] pieces = board.GetPrevTurnPieces(); // we'll calculate heuristics for the player who got us here
+		BoardPiece[] pieces = board.GetTurnPieces(); // we'll calculate heuristics for the player who's turn it is
 		int[] positions = new int[pieces.length];
 		for (int i = 0; i < 4; ++i) {
 			int index = pieces[i].pos.CalculateIndex();
@@ -119,19 +150,7 @@ public class Heuristics {
 		return first_degree;
 	}
 
-	public static double GetCountHeuristic(LocalState board){
-		BoardPiece[] pieces = board.GetPrevTurnPieces(); // we'll calculate heuristics for the player who got us here
-		CountData total = new CountData();
-		for (int i = 0; i < 4; ++i) {
-			int index = pieces[i].pos.CalculateIndex();
-			total.add(GetCount(board,index));
-		}
-		total.empty_heuristic /= (4*127);
-		total.nonempty_heuristic /= (4*80);
-		return ASingleMaths.remap_value(total.empty_heuristic - total.nonempty_heuristic,-1,1,0,1);
-	}
-
-	static CountData GetCount(LocalState board, int startingPos) {
+	public static CountData GetCount(LocalState board, int startingPos) {
 		CountData counts = new CountData();
 		CountingAlgorithmData data = new CountingAlgorithmData();
 		Position start = new Position(startingPos);
@@ -148,11 +167,8 @@ public class Heuristics {
 		if (!data.blankspace.isEmpty()) {
 			while (!data.blankspace.isEmpty()) {
 				int value = data.blankspace.poll();
-				counts.empty++;
+				counts.our_empty++;
 				QueueNeighbours(data, value, board);
-
-				// todo (debug): tune this weighting
-				counts.empty_heuristic += is_first_degree.apply(start,value) ? 1.8 : 1;
 			}
 		}
 
@@ -160,11 +176,11 @@ public class Heuristics {
 			while (!data.blockedspace.isEmpty()) {
 				int value = data.blockedspace.poll();
 				Position current = new Position(value);
-				counts.nonempty++;
+				counts.our_nonempty++;
 
 				//calculate simple distance
 				int max = Math.max(Math.abs(start.x - current.x),  Math.abs(start.y - current.y));
-				counts.nonempty_heuristic += 10.0/Math.pow(10,max-1);
+				counts.our_nonempty_weighted += 10.0/Math.pow(10,max-1);
 			}
 		}
 		return counts;
@@ -195,39 +211,30 @@ public class Heuristics {
 			}
 		}
 	}
-	
-	public static double GetTerritoryHeuristic(LocalState board){
-		var counts = GetTerritoryCount(board);
-		int total = counts.ours + counts.theirs;
-		double heuristic = 3 * ((double)counts.ours / total);
-		heuristic = Math.pow(heuristic,2);
-		heuristic /= 9;
-		return Math.max(0,Math.min(heuristic,1));
-	}
 
 	public static TerritoryCounts GetTerritoryCount(LocalState board){
 		BoardPiece[] our_pieces = board.GetPrevTurnPieces();
 		BoardPiece[] their_pieces = board.GetTurnPieces();
 		int[] our_positions = new int[4];
 		int[] their_positions = new int[4];
-		int[] our_degree_counts = new int[121];
-		int[] their_degree_counts = new int[121];
+		int[] our_degree_map = new int[121];
+		int[] their_degree_map = new int[121];
 
 		for(int i = 0; i < 4; i++) {
 			our_positions[i] = our_pieces[i].pos.CalculateIndex();
 			their_positions[i] = their_pieces[i].pos.CalculateIndex();			//element # is the index
 		}
 
-		GetTerritoryCount(board, our_positions, our_degree_counts, 1, new TreeSet<Territory>(new Territory.TerritoryComp()));
-		GetTerritoryCount(board, their_positions, their_degree_counts, 1, new TreeSet<Territory>(new Territory.TerritoryComp()));
+		GetTerritoryCount(board, our_positions, our_degree_map, 1, new TreeSet<Territory>(new Territory.TerritoryComp()));
+		GetTerritoryCount(board, their_positions, their_degree_map, 1, new TreeSet<Territory>(new Territory.TerritoryComp()));
 
 		int our_territory_count = 0;
 		int their_territory_count = 0;
 
-		for(int i = 0; i < our_degree_counts.length; i++) {
-			if(our_degree_counts[i] < their_degree_counts[i]) {
+		for(int i = 0; i < our_degree_map.length; i++) {
+			if(our_degree_map[i] < their_degree_map[i]) {
 				our_territory_count++;
-			} else if(our_degree_counts[i] > their_degree_counts[i]){
+			} else if(our_degree_map[i] > their_degree_map[i]){
 				their_territory_count++;                               /* if we have a lower cost to move set to us, else set to 0 */
 			}
 		}
@@ -235,7 +242,7 @@ public class Heuristics {
 		return new TerritoryCounts(our_territory_count,their_territory_count);
 	}
 
-	private static void GetTerritoryCount(LocalState board, int[] starting_positions, int[] degrees, int degree, TreeSet<Territory> visited) {
+	private static void GetTerritoryCount(LocalState board, int[] starting_positions, int[] degree_mapping, int degree, TreeSet<Territory> visited) {
 		int[][] new_positions = MoveCompiler.GetOpenPositions(board, starting_positions); //[starting index][index of open positions]
 
 		for (int[] position_list : new_positions) {
@@ -244,8 +251,8 @@ public class Heuristics {
 					if (position_list[j] == -1) {
 						break;
 					}
-					if (degrees[position_list[j]] == 0 || degrees[position_list[j]] > degree) {
-						degrees[position_list[j]] = degree;
+					if (degree_mapping[position_list[j]] == 0 || degree_mapping[position_list[j]] > degree) {
+						degree_mapping[position_list[j]] = degree;
 					}
 				}
 			}
@@ -253,7 +260,7 @@ public class Heuristics {
 
 		for (int[] position_list : new_positions) {
 			if(position_list != null) {
-				GetTerritoryCount(board, prune_positions(visited, position_list, degree), degrees, degree + 1, visited);
+				GetTerritoryCount(board, prune_positions(visited, position_list, degree), degree_mapping, degree + 1, visited);
 			}
 		}
 	}
@@ -282,15 +289,21 @@ public class Heuristics {
 	}
 
 	public static class CountData {
-		public int empty;
-		public int nonempty;
-		public double empty_heuristic;
-		public double nonempty_heuristic;
-		void add(CountData other){
-			empty += other.empty;
-			empty_heuristic += other.empty_heuristic;
-			nonempty += other.nonempty;
-			nonempty_heuristic += other.nonempty_heuristic;
+		public int our_empty;
+		public int our_nonempty;
+		public int their_empty;
+		public int their_nonempty;
+		public double our_nonempty_weighted;
+		public double their_nonempty_weighted;
+		void add_our_counts(CountData other){
+			our_empty += other.our_empty;
+			our_nonempty += other.our_nonempty;
+			our_nonempty_weighted += other.our_nonempty_weighted;
+		}
+		void add_their_counts(CountData other){
+			their_empty += other.our_empty;
+			their_nonempty += other.our_nonempty;
+			their_nonempty_weighted += other.our_nonempty_weighted;
 		}
 	}
 
