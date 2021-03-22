d8d9f318ad10b9fd0638ab6a9deae90bcf3b2943
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-22 14:45:49 -0700

    Updates MovePool.generate_pool

--- src/main/java/structures/MovePool.java
+++ src/main/java/structures/MovePool.java
@@ -9,21 +9,8 @@ public class MovePool {
     private static HashMap<Integer, Move> pool = new HashMap<>(1200000, 1.f);
 
     public static void generate_pool(){
-        BoardPiece[] all_positions = new BoardPiece[100];
-        int i = 0;
-        for(int x = 1; x < 11; ++x){
-            for(int y = 1; y < 11; ++y){
-                Position p = new Position(x,y);
-                if(p.IsValid()){
-                    all_positions[i++] = new BoardPiece(p.x,p.y,1);
-                }
-            }
-        }
-        ArrayList<Integer> blank_state = new ArrayList<Integer>(121);
-        for(int j = 0; j < 121; ++j){
-            blank_state.add(0);
-        }
-        MoveCompiler.GetMoveList(new LocalState(blank_state,false,true),all_positions,true);
+        int[] all_positions = Debug.GetAllPositions();
+        MoveCompiler.GetMoveList(new LocalState(),all_positions,true);
     }
 
     private static int make_key(int start, int next, int arrow){
