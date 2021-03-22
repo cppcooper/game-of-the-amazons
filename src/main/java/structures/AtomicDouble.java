72bce386f181f4b8eebf4b237d24dbda391c27ee
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-21 23:04:31 -0700

    Adds a cached double to view the AtomicDouble value in debugger

--- src/main/java/structures/AtomicDouble.java
+++ src/main/java/structures/AtomicDouble.java
@@ -4,10 +4,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class AtomicDouble {
     private AtomicLong value = new AtomicLong(Double.doubleToLongBits(0.0));
+    private volatile double cached = 0.0;
     public double get(){
         return Double.longBitsToDouble(value.get());
     }
     public void set(double new_value){
+        cached = new_value;
         value.set(Double.doubleToLongBits(new_value));
     }
 }
