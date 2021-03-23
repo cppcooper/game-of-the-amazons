17a2344179e70f2128f5c9fe9a39af427b83f1df
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-22 22:12:09 -0700

    Adds lerp function for blending heuristics

--- src/main/java/tools/ASingleMaths.java
+++ src/main/java/tools/ASingleMaths.java
@@ -4,5 +4,9 @@ public class ASingleMaths {
     public static double remap_value(double value, double r1_low, double r1_high, double r2_low, double r2_high) {
         return r2_low + (value - r1_low) * (r2_high - r2_low) / (r1_high - r1_low);
     }
-
+    public static double lerp(double v0, double v1, double alpha){
+        double min = Math.min(v0,v1);
+        double max = Math.max(v0,v1);
+        return Math.min(max,Math.max(min,min + (alpha * (max-min))));
+    }
 }
