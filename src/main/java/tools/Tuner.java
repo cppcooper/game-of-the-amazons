742dacc0de55d9349fb86241c5dcc196348191d3
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-29 23:36:05 -0700

    Adds heuristic tuning variables

--- src/main/java/tools/Tuner.java
+++ src/main/java/tools/Tuner.java
@@ -1,9 +1,14 @@
 package tools;
 
 public class Tuner {
-    public static final int wait_time = 1000 * 5;
-    public static final double t1c = 1.0;
-    public static final double t2c = 1.0;
-    public static final double c1c = 1.0;
+    public static final int wait_time = 1000 * 3;
+    public static final double t1c = 7.0;
+    public static final double c1c = 10.0/8.0;
     public static final double c2c = 1.0;
+    public static final double t2c = 3.0/5.0;
+
+    public static final double t1b = 0.0;
+    public static final double c1b = 0.1;
+    public static final double c2b = -0.1;
+    public static final double t2b = 0.0;
 }
