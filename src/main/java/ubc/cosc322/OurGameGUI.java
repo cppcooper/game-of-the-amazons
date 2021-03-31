0d46052098a2f7d50a9e9f0e4e9d1b21094049ad
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-31 16:02:26 -0700

    Adds correct shutdown code

--- src/main/java/ubc/cosc322/OurGameGUI.java
+++ src/main/java/ubc/cosc322/OurGameGUI.java
@@ -3,15 +3,50 @@ package ubc.cosc322;
 import ygraph.ai.smartfox.games.BaseGameGUI;
 import ygraph.ai.smartfox.games.GamePlayer;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+
 public class OurGameGUI extends BaseGameGUI {
 
     public OurGameGUI(GamePlayer player) {
         super(player);
-    }
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("window closing");
+                AICore.TerminateThreads();
+            }
 
-    @Override
-    public void dispose() {
-        super.dispose();
-        AICore.TerminateThreads();
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
     }
 }
