60466164b2c42785f906b5ef23e8533b8584b4c3
Josh Cooper <cooper.s.josh@outlook.com>
2021-05-08 16:32:09 -0700

    Simplifies best node search

--- src/solution/java/algorithms/search/BestNode.java
+++ src/solution/java/algorithms/search/BestNode.java
@@ -8,103 +8,35 @@ import tools.Tuner;
 public class BestNode {
     protected Benchmarker B = new Benchmarker();
     protected GameTreeNode best_node = null;
-    protected int depth_reached = 0;
 
     public static GameTreeNode Get(GameTreeNode root) {
         return new BestNode(root).best_node;
     }
 
     protected BestNode(GameTreeNode root){
-        if(!root.isTerminal()) {
-            best_node = find_best_node(root, true);
-            if (best_node == null) {
-                best_node = find_best_node(root, false);
-            }
-        }
+        B.start();
+        findBest(root);
     }
 
-    private GameTreeNode find_best_node(GameTreeNode root, boolean is_first_call){
-        Best best = new Best();
-        if (root == null) {
-            Debug.NoParentNodeFound.set(true);
-            System.out.println("BestMove.Get: GameTree can't find the state");
-            throw new IllegalStateException("There absolutely should be a root node, and we can't find it.");
-        } else if (root.edges() == 0) {
-            Debug.ZeroEdgesDetected.set(true);
-            System.out.println("BestMove.Get: Zero edges");
-//            throw new IllegalStateException(
-//                    String.format("This should mean we have lost the game, in which case this thread should have been terminated.\n" +
-//                            "[game can continue: %B]", root.state_after_move.get().CanGameContinue()));
-        }
-        Debug.RunInfoL2DebugCode(() -> System.out.printf("BestMove.Get: our root node has %d edges, now to find the best edge\n", root.edges()));
-        for (int i = 0; i < root.edges(); ++i) {
-            if(B.Elapsed() < Tuner.max_wait_time){
-                GameTreeNode selection = root.get(i);
-                if (!selection.isReady()) {
-                    Debug.RunVerboseL1DebugCode(() -> System.out.printf("BestMove.Get: node not ready. [Node: %s]\n", selection));
-                    selection.evaluate();
-                }
-                final int edge = i;
-                Debug.RunVerboseL1DebugCode(() -> System.out.printf("BestMove.find_best_node: node %d\n%s", edge, selection));
-                best.node = which_best(best, selection, is_first_call);
-                continue;
-            }
-            break;
-        }
-        // We've found our best nodes, now we need to return
-        if (best.node != null) {
-            System.out.println("BestMove.find_best: found one");
-            return best.node;
-        }
-        return null;
-    }
-
-    private GameTreeNode which_best(Best best, GameTreeNode selection, boolean use_recursion){
-        double aggregate = Double.NEGATIVE_INFINITY;
-        double heuristic;
-        if(use_recursion) {
-            depth_reached = 0; //get_heuristic() will update this variable
-            heuristic = evaluate(selection, 1) / depth_reached;
-        } else {
-            heuristic = evaluate(selection, Tuner.max_search_depth - 1);
-        }
-        if (heuristic > 0) {
-            if (heuristic >= best.value && aggregate >= best.agg) {
-                best.value = heuristic;
-                best.agg = aggregate;
-                return selection;
-            }
-        }
-        return best_node;
-    }
-
-    private double evaluate(GameTreeNode node, int depth){
-        double heuristic = 0;
-        if (depth < Tuner.max_search_depth) {
-            // Recursively sum the best nodes' heuristics
-            if (node != null && node.isReady()) {
-                depth_reached = depth;
-                heuristic = node.combined.get() - node.maximum_sub.get();
-                if(heuristic < 0){
-                    // if we calculate a negative value, then this node is no good
-                    if (depth == 1){
-                        return Double.NEGATIVE_INFINITY;
+    private void findBest(GameTreeNode root){
+        if(!root.isTerminal() && root.isReady()) {
+            double best = Double.NEGATIVE_INFINITY;
+            for (int i = 0; i < root.edges(); ++i) {
+                if(B.elapsed() < Tuner.max_wait_time) {
+                    GameTreeNode child = root.get(i);
+                    if (child.isTerminal()) {
+                        best_node = child;
+                        return;
                     }
-                    depth_reached--;
-                    return 0;
+                    if (child.isReady()) {
+                        double h = root.combined.get() - child.combined.get() + (child.maximum_sub.get() / 10.0);
+                        if (h > best) {
+                            best = h;
+                            best_node = child;
+                        }
+                    }
                 }
-                heuristic += evaluate(find_best_node(node, false), depth + 1);
-            } else if (depth == 1) {
-                // we don't have heuristic information available for this node
-                return Double.NEGATIVE_INFINITY;
             }
         }
-        return heuristic;
-    }
-
-    private class Best{
-        private GameTreeNode node = null;
-        private double value = Double.NEGATIVE_INFINITY;
-        private double agg = Double.NEGATIVE_INFINITY;
     }
 }
